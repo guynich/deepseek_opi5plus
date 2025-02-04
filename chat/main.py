9917abbf10a1a946bc6f58984dde566d410cd4a0
Guy Nicholson <guy.nicholson@gmail.com>
2025-02-04 12:39:55 -0800

    Refactor chat script
    Reduce code verbosity.  Add comments.  Rename class and variables.

--- chat/main.py
+++ chat/main.py
@@ -1,47 +1,42 @@
-"""Script for contextual chatting with the DeepSeek R1 model."""
+"""Script for chatting with history using distilled version of DeepSeek R1 model."""
 
-from typing import List
-
-import ollama
+from ollama import chat
 
 MODEL = "deepseek-r1:1.5b"
-# Some of these prompt examples assume context is maintained.
+# Some of these prompts require history to generate a response.
 PROMPTS = [
     "What is the sum of 3 + 2?",
     "What is the result if +1 is added to both numbers before summing them?",
     "What is the capital of France?",
-    "Describe briefly a historical event from that city.",
+    "Describe an historical event from the 18th century in that city.",
 ]
 
 
-class DeepSeekChat:
+class OllamaChat:
+    """A chat interface with history for Ollama large language models."""
+
     def __init__(self, model: str):
         self.model = model
         self.messages = []
 
-    def __call__(self, prompt_text):
-        self.messages.append({"role": "user", "content": prompt_text})
+    def __call__(self, prompt):
+        self.messages.append({"role": "user", "content": prompt})
 
-        response = ollama.chat(model=self.model, messages=self.messages)
-        self.messages.append(
-            {"role": "assistant", "content": response["message"]["content"]}
-        )
+        response = chat(model=self.model, messages=self.messages)
 
-        return response["message"]["content"]
-
-
-def main(prompts: List[str]) -> None:
-    chat = DeepSeekChat(model=MODEL)
-    for prompt in prompts:
-        try:
-            print(f"User:  {prompt}")
-            print(f"Assistant:\n{chat(prompt)}\n")
-        except Exception as e:
-            print(f"An error occurred: {e}")
-        except KeyboardInterrupt:
-            print("User quit.")
-            break
+        self.messages.append({"role": "assistant", "content": response.message.content})
+        return response.message.content
 
 
 if __name__ == "__main__":
-    main(PROMPTS)
+    deepseek_chat = OllamaChat(model=MODEL)
+
+    for prompt in PROMPTS:
+        try:
+            print(f"User:  {prompt}\nRunning model ...")
+            print(f"Assistant:\n{deepseek_chat(prompt)}\n")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+        except KeyboardInterrupt:
+            print("\nUser quit.")
+            break
